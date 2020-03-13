7d12544b3a6c18e7d5a09e398171aeb62fe03eb9
Kevin Hoser <hoser21@up.edu>
2020-03-13 10:12:35 -0700

    Add init dynamic solution

---
+++ dynamic_knapsack.py
@@ -0,0 +1,17 @@
+def knapsack_r(w, weights, vals, index):
+    pass
+
+
+def knapsack(w, weights, vals):
+    pass
+
+
+def main():
+    weights = [1, 2, 3, 1]
+    vals = [6, 10, 12, 7]
+    w = 5
+    print(knapsack(w, weights, vals))
+
+
+if __name__ == "__main__":
+    main()
